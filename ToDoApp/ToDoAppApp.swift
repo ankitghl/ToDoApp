62308452999c48a2175bfc11296560f2819b59bf
ankitghl <gankit27@gmail.com>
2025-07-15 23:24:03 +0530

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Ankit Gohel on 15/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
